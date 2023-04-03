81eb2bc019c14efde9dd3eaaf302494d376f6c9d
Andrew Sanchez <asanchez@yext.com>
2023-04-03 10:58:56 -0400

    update deployer

--- deployer/deploy_sites.py
+++ deployer/deploy_sites.py
@@ -1,13 +1,19 @@
 import requests as re
+import random
 from datetime import datetime
 
 url = "https://api.yext.com/v2/accounts/me/resourcesapplyrequests?v=20220101&api_key=371ba6333664664048b449c5917ecb56"
+ids = input(
+    'Enter sub account ids separated by commas or "s" for a subset of 61 accts: '
+)
 
-ids = input('Enter the sub account ids separated by commas: ')
-
-if ids != "":
+child_ids = []
+if ids == "s":
+    with open("./subset_accounts.txt", "r") as f:
+        child_ids = f.readlines()
+elif ids != "":
     child_ids = ids.split(",")
-else: 
+else:
     with open("./sub_accounts.txt", "r") as f:
         child_ids = f.readlines()
 
@@ -25,10 +31,10 @@ def build_site_body(sub_account_id):
             "type": "GitHub",
             "url": "https://github.com/lambdaFun94/cac-pages-yextsite-config",
             "variables": {
-                "siteId": "site-id-" + dt_string_1,
+                "siteId": "site-id-" + dt_string_1 + str(random.randint(1,100)),
                 "siteName": "Deployed at " + dt_string,
-                "repoId": "basic-locations-repo",
-                "gitHubUrl": "github.com/lambdaFun94/avs-basic-locations-fork"
+                "repoId": "basic-locations-repo-fleet",
+                "gitHubUrl": "github.com/lambdaFun94/basic-locations-site"
             }
         }
     }
